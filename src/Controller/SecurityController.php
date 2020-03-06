ae00dbb8650d0e6e90ffd18dce35ebfbe939f207
coferrete <co.ferrete@atlanctic.es>
2020-03-06 09:53:28 +0100

    Arreglar problemas

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -32,7 +32,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
     /**
      * @Route("/security_check", name="security_check")
