45a121ecf5382507802dbe67ecdb47158ac4c016
José Ignacio Fernández Seda <ignacio.fernandez@atlantic.es>
2020-11-19 15:46:24 +0300

    Intentar mejorar la cache

--- cronjobs/semana/deletecacheprod.sh
+++ cronjobs/semana/deletecacheprod.sh
@@ -4,3 +4,11 @@
 #
 
 /opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console cache:clear --env=prod
+
+/opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console cache:pool:clear cache.app
+/opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console cache:pool:clear cache.system
+/opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console cache:pool:clear cache.validator
+/opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console cache.annotations
+/opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console doctrine.result_cache_pool
+/opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console doctrine.system_cache_pool
+/opt/rh/rh-php72/root/usr/bin/php /opt/app-root/src/bin/console cache.property_access
