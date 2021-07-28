950a343410148fd6cb67fe683fd6300ded122fe7
manuel <manuel.navarro@atlantic.es>
2021-07-28 09:17:41 +0200

    correccion4

--- src/Command/EmaildoccarSend4Command.php
+++ src/Command/EmaildoccarSend4Command.php
@@ -47,7 +47,7 @@ class EmaildoccarSend4Command extends Command
      */
     protected function configure()
     {
-        $this
+        $this->setName('email:emaildoccar4:send') 
             ->setDescription('Send simple email message')
             ->addOption('from', null, InputOption::VALUE_REQUIRED, 'The from address of the message')
             ->addOption('to', null, InputOption::VALUE_REQUIRED, 'The to address of the message')
